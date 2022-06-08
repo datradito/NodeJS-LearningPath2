c239967c0b3a603e23d00b8386356128ce4485c4
begomib <begomi@gmail.com>
2022-06-08 09:44:26 -0300

    unid7_pug

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,36 @@
 const path= require('path');
 
-const rootDir = require('../util/path');
+
+const productsController = require('../controllers/products');
+//const rootDir = require('../util/path');
 
 const express= require('express');
 const { title } = require('process');
 
 const router = express.Router();
 
-const products = [];
+//const products = [];
 
 //admin/add-product =>GET
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product',{ pageTitle:'Add Product', path:'/admin/add-product'});
-});  
+router.get('/add-product', productsController.getAddProduct);
+
+//(req, res, next) => {
+  //  res.render('add-product',{ pageTitle:'Add Product', path:'/admin/add-product'});
+//});  
 
 
 //admin/add/add-product => POST
-router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-   products.push({title: req.body.title});
-    res.redirect('/');
-});
+router.post('/add-product',productsController.postAddProduct);
 
-exports.routes = router;
-exports.products =products;
+//(req, res, next) => {
+  //  console.log(req.body);
+   //products.push({title: req.body.title});
+    //res.redirect('/');
+//
+//exports.routes = router;
+//exports.products =products;
+
+module.exports=router;
+
+
 
