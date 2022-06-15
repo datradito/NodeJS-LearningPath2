142985f6d0e4033a59d8eb8d2c1e2e94e071e149
datradito <datradito@gmail.com>
2022-06-15 15:43:12 -0300

    fix: minor typo

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 //conectar con la base de datos de mongoos
-mongoose.connect(process.env.MONGODB_URI, 
-{   useNewUrlParser: true,
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
-})
-.then(() => console.log("Database Connectec"))
-.catch(error => console.log(error))
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((error) => console.log(error));
